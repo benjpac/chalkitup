cb82fc49525e54001eae12e924f403e8587b5cb3
Ben Metzger <benjpac@gmail.com>
2017-06-03 16:32:29 -0700

    add components for new posts

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      rentals: this.store.findAll('post'),
-      reviews: this.store.findAll('review')
+      posts: this.store.findAll('post'),
+      comments: this.store.findAll('comment')
     });
   },
+  actions: {
+    savePost3(params) {
+      var newPost = this.store.createRecord('post', params);
+      newPost.save();
+      this.transitionTo('index');
+    }
+  }
 });
