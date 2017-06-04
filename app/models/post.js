a33667c0b78f11d68031fd35b9d564cee2e5a11f
Ben Metzger <benjpac@gmail.com>
2017-06-03 20:03:18 -0700

    add detail attr to post model

--- app/models/post.js
+++ app/models/post.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr(),
   author: DS.attr(),
+  detail: DS.attr(),
   created: DS.attr({
     defaultValue(){
       return new Date();
